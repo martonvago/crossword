d7be53daebf82764288871d084c7633aaa142d12
Matthew Wilson <Matthew.Wilson@softwire.com>
2020-04-17 16:12:10 +0100

    Created clue model & repository

---
+++ src/main/java/com/example/crossword/models/Clue.java
@@ -0,0 +1,72 @@
+package com.example.crossword.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class Clue {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer id;
+
+    private String clueText;
+
+    private String topic;
+
+    private String style;
+
+    private String author;
+
+    private String difficulty;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getClueText() {
+        return clueText;
+    }
+
+    public void setClueText(String clueText) {
+        this.clueText = clueText;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public void setStyle(String style) {
+        this.style = style;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(String difficulty) {
+        this.difficulty = difficulty;
+    }
+}
---
+++ src/main/java/com/example/crossword/repositories/ClueRepository.java
@@ -0,0 +1,8 @@
+package com.example.crossword.repositories;
+
+import com.example.crossword.models.Clue;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ClueRepository extends CrudRepository<Clue, Integer> {
+
+}
